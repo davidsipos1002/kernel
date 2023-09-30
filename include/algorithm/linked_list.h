2be8180ab99ae8e44826d384b96a1a60b574c98f
David Oliver Sipos <davidsipos1002@gmail.com>
2023-09-30 23:23:48 +0300

    Algorithm: implement xor list

---
+++ include/algorithm/linked_list.h
@@ -0,0 +1,27 @@
+#ifndef ALGORITHM_LINKED_LIST_H_INCL
+#define ALGORITHM_LINKED_LIST_H_INCL
+
+#include <stdint.h>
+
+typedef struct 
+{
+    uint64_t key;
+    uint64_t link;
+} linked_list_node;
+
+typedef struct
+{
+    uint64_t head;
+    uint64_t last;
+} linked_list;
+
+void linked_list_init(linked_list *list);
+void linked_list_insert_front(linked_list *list, linked_list_node *node);
+void linked_list_insert_rear(linked_list *list, linked_list_node *node);
+linked_list_node* linked_list_find(linked_list *list, uint64_t key);
+void linked_list_insert_after(linked_list *list, uint64_t after, linked_list_node *node);
+void linked_list_delete_first(linked_list *list);
+void linked_list_delete_last(linked_list *list);
+uint8_t linked_list_delete(linked_list *list, uint64_t key);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <paging/paging.h>
 #include <memory/simple_allocator.h>
 #include <memory/memory_map.h>
+#include <algorithm/linked_list.h>
 
 extern char __kernel_data_begin[];
 extern char __kernel_data_end[];
@@ -27,6 +28,23 @@ int kernel_main(BootInfo *bootInfo)
             *((uint32_t *) (buffer + bootInfo->framebuffer.pixel_size * i)) = 0xFF00;
         }
     }
+    start = (uint64_t) __kernel_data_begin + sizeof(paging_state);
+    linked_list list;
+    simple_allocator *alloc = simple_allocator_init((void *) start, 105 * sizeof(linked_list_node));
+    linked_list_init(&list);
+    linked_list_node *a = simple_allocator_alloc(alloc, sizeof(linked_list_node));
+    a->key = 10;
+    linked_list_insert_front(&list, a);
+    linked_list_node *b = simple_allocator_alloc(alloc, sizeof(linked_list_node));
+    b->key = 20;
+    linked_list_insert_front(&list, b);
+    linked_list_node *c = simple_allocator_alloc(alloc, sizeof(linked_list_node));
+    c->key = 15;
+    linked_list_insert_front(&list, c);
+    linked_list_node *find = linked_list_find(&list, 20);
+    linked_list_delete(&list, 20);
+    find = linked_list_find(&list, 20);
+    find = linked_list_find(&list, 15);
     while (1);
     return 0;
 }

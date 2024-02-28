78ca77cf1a05ccc319c40b6ad7f3e04b00683455
David Oliver Sipos <davidsipos1002@gmail.com>
2024-02-29 00:38:45 +0200

    Update firmware image, built on Windows 11

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 echo 'Starting QEMU...'
-qemu-system-x86_64 -cpu Skylake-Client-v4,vendor=GenuineIntel,pge=on,pks=on,pku=on,rdrand=on,rdseed=on -monitor stdio -s -S -bios external/uefi/UEFI64.bin -drive file=img/boot.cdr,format=raw -m 4G -d cpu_reset 
+qemu-system-x86_64 -cpu Skylake-Client-v4,vendor=GenuineIntel,pge=on,pks=on,pku=on,rdrand=on,rdseed=on -monitor stdio -s -S -bios external/uefi/OVMF.fd -drive file=img/boot.cdr,format=raw -m 4G -smp 4 -d cpu_reset 
